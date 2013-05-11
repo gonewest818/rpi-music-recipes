396e13d017b0f563e62bf088a93e4b4962089196
Neil Okamoto <pi.gonewest@xoxy.net>
2013-05-11 12:54:30 -0700

    run shairport under the name "AirPi"

--- pi-shairport.sh
+++ pi-shairport.sh
@@ -5,6 +5,9 @@ apt-get --yes upgrade
 #apt-get --yes install pulseaudio
 sudo apt-get install git libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils libmodule-build-perl
 
+# init scripts
+cp shairport.init /etc/init.d/shairport
+
 cd /tmp
 git clone https://github.com/njh/perl-net-sdp.git perl-net-sdp
 cd perl-net-sdp
@@ -17,7 +20,6 @@ cd /tmp
 git clone https://github.com/hendrikw82/shairport.git
 cd shairport
 make install
-cp shairport.init.sample /etc/init.d/shairport
 chmod a+x /etc/init.d/shairport
 update-rc.d shairport defaults
 
