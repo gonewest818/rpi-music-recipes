c0a3497c2df6bdf4683d5da2cb0da27af1d73717
Neil Okamoto <pi.gonewest@xoxy.net>
2014-01-12 13:37:10 -0800

    fix configuration for new shairport

--- pi-shairport2.sh
+++ pi-shairport2.sh
@@ -2,19 +2,26 @@
 
 apt-get --yes update
 apt-get --yes upgrade
-apt-get install libao-dev libssl-dev libavahi-client-dev libasound2-dev
+apt-get install libao-dev libssl-dev libavahi-client-dev libasound2-dev avahi-daemon
 
-#package
+# configuration
+cp shairport2.default /etc/default/shairport
+
+# alsa
+amixer cset numid=3 1
+# may need to edit alsa.conf also
+
+# shairport package
 cd /tmp
 git clone https://github.com/abrasive/shairport.git
 cd shairport
 ./configure
-make install
+PREFIX=/usr make install
 chmod a+x /etc/init.d/shairport
 
 # init scripts
-cp scripts/debian/default/shairport /etc/default/shairport
 cp scripts/debian/init.d/shairport /etc/init.d/shairport
 cp scripts/debian/logrotate.d/shairport /etc/logrotate.d/shairport
 update-rc.d shairport defaults
+
 
