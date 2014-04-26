575ea054f184a550711a4aee739a0f615dacf33d
Neil Okamoto <neil.okamoto@gmail.com>
2014-04-26 08:33:31 -0700

    tweaking configs

--- shairport/pi-shairport2.sh
+++ shairport/pi-shairport2.sh
@@ -6,6 +6,7 @@ apt-get install libao-dev libssl-dev libavahi-client-dev libasound2-dev avahi-da
 
 # configuration
 cp shairport2.default /etc/default/shairport
+cp logrotate.shairport /etc/logrotate.d/shairport
 
 # alsa
 amixer cset numid=3 1
@@ -21,7 +22,6 @@ PREFIX=/usr make install
 # init scripts
 cp scripts/debian/init.d/shairport /etc/init.d/shairport
 chmod a+x /etc/init.d/shairport
-cp scripts/debian/logrotate.d/shairport /etc/logrotate.d/shairport
 update-rc.d shairport defaults
 update-rc.d avahi-daemon defaults
 
